32f743a810ffbcbe0a47bb5b87ca9506ef347e83
andresLnx <andreserluis@gmail.com>
2022-10-16 21:38:02 -0400

    create(Login Component and the custom hook  useForm())

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,7 +1,41 @@
 import React from "react";
+import { useForm } from "../../hooks/useForm";
 
 const Login = () => {
-  return <div>Login</div>;
+  //  Hooks and State
+  const [values, handleOnChange] = useForm({ username: "", password: "" });
+  const { username, password } = values;
+
+  // Handlers
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    alert(JSON.stringify(values));
+  };
+  return (
+    <div>
+      <form onSubmit={handleOnSubmit}>
+        <div>
+          <input
+            name="username"
+            type="text"
+            value={username}
+            placeholder="username"
+            onChange={handleOnChange}
+          />
+        </div>
+        <div>
+          <input
+            name="password"
+            type="passsword"
+            value={password}
+            placeholder="passsword"
+            onChange={handleOnChange}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
